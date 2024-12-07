1b0db42a594db55dad67bfd4810203db5de18b50
unknown <vpk5588@gmail.com>
2024-12-07 16:48:15 +0800

    Adding the missed file

--- src/main/java/com/hcl/hackaton/services/MortgageService.java
+++ src/main/java/com/hcl/hackaton/services/MortgageService.java
@@ -1,14 +1,10 @@
 package com.hcl.hackaton.services;
 
 import com.hcl.hackaton.dto.MortgageDTO;
-import com.hcl.hackaton.entity.Account;
 import com.hcl.hackaton.entity.Mortgage;
-import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
 public interface MortgageService {
 
     public abstract BigDecimal showAccountBalance();
