533aed4340579ff0a38dc28e88e52d7fe83e19bc
Anurag <anurag308@gmail.com>
2024-12-07 12:31:15 +0530

    customer servuce

--- src/main/java/com/hcl/hackaton/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/hcl/hackaton/services/impl/CustomerServiceImpl.java
@@ -3,12 +3,10 @@ package com.hcl.hackaton.services.impl;
 import com.hcl.hackaton.dto.CustomerDTO;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hcl.hackaton.entity.Customer;
 import com.hcl.hackaton.repository.CustomerRepository;
-import com.hcl.hackaton.services.CustomerService;
 
 @Service
 public class CustomerServiceImpl {
@@ -21,13 +19,12 @@ public class CustomerServiceImpl {
         this.customerRepo = customerRepo;
     }
     
-	@Override
+	
 	public Customer createCustomer(Customer customer) {
 		Customer cust = customerRepo.save(customer);
 		return cust;
 	}
 
-	@Override
 	public Customer getCustomer(long customerId) {
 		Optional<Customer> cust = customerRepo.findById(customerId);
 		if(cust.isPresent()) {
@@ -36,7 +33,6 @@ public class CustomerServiceImpl {
 		return null;
 		}
 
-	@Override
 	public void deleteCustomer(long customerId) {
 		customerRepo.deleteById(customerId);
 	}
