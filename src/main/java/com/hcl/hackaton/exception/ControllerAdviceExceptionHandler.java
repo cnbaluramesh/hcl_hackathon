91ee14ba25367354af8469c82a7138cb39c35636
Anurag <anurag308@gmail.com>
2024-12-07 12:45:44 +0530

    added missed import

--- src/main/java/com/hcl/hackaton/exception/ControllerAdviceExceptionHandler.java
+++ src/main/java/com/hcl/hackaton/exception/ControllerAdviceExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
+import com.hcl.hackaton.exception.ErrorMessage;
 @ControllerAdvice
 public class ControllerAdviceExceptionHandler {
 
